75465bd79bcd87a6589b83fbc1f9fe854034f2ea
Ben Bavar <ben.bavar@gmail.com>
2023-08-16 17:30:57 -0400

    Sent waitUntilExit message to makeReadable task to make sure read permission was added to blocklist prior to attempt to produce string from its contents

--- wxWidgets-3.2.2.1/mac-build/obj-c/main.m
+++ wxWidgets-3.2.2.1/mac-build/obj-c/main.m
@@ -4,14 +4,21 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         BNRLogger *logger = [[BNRLogger alloc] init];
         
-        NSTask *task = [[NSTask alloc] init];
-        [task setLaunchPath:@"/bin/chmod"];
-        [task setArguments:@[@"+r", @"../.blocklist.txt"]];
-        [task launch];
+        NSTask *makeReadable = [[NSTask alloc] init];
+        [makeReadable setLaunchPath:@"/bin/chmod"];
+        [makeReadable setArguments:@[@"+r", @"../.blocklist.txt"]];
+        [makeReadable launch];
+        [makeReadable waitUntilExit];
 
         NSString *string = [NSString stringWithContentsOfFile:@"../.blocklist.txt"
                                                      encoding:NSASCIIStringEncoding
                                                         error:NULL];
+        
+        NSTask *makeUnreadable = [[NSTask alloc] init];
+        [makeUnreadable setLaunchPath:@"/bin/chmod"];
+        [makeUnreadable setArguments:@[@"-r", @"../.blocklist.txt"]];
+        [makeUnreadable launch];
+        
         NSLog(@"string = %@", string);
         [logger setBlocklist:string];
 
